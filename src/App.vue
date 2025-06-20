92ccd5d67b237598ca937d10eecc63cba247960d
Divya Prakash Gupta <divyapg07@gmail.com>
2025-06-20 16:32:33 +0200

    feat: initialized basic vue.js application and configured build using vite

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>Hello World</h1>
+</template>
+
+<style scoped>
+  h1 {
+    color: #42b883;
+    font-size: 2em;
+    text-align: center;
+    margin-top: 20px;
+  }
+</style>
